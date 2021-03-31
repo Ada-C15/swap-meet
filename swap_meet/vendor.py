e880d359f395d8a458f54babdbcc5e2b7a887c54
Christian <Codedreamer20@gmail.com>
2021-03-31 16:46:25 -0700

    wave 01 test passing

---
+++ swap_meet/vendor.py
@@ -0,0 +1,22 @@
+class Vendor:
+    def __init__(self, inventory = None):
+        self.inventory = inventory
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+
+    def remove(self, item_to_remove):
+        if item_to_remove not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item_to_remove)
+            return item_to_remove
+         
+
+        
+            
