2f5313289a0c70ae1800c413ec8e2a69aeb2ad69
Courtney <courtneycmassey@gmail.com>
2021-03-31 14:46:25 -0700

    passing Wave01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,23 @@
 class Vendor:
 
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
         if not inventory:
             self.inventory = []
         else:
             self.inventory = inventory 
+
+    def add(self, new_item):
+        self.inventory.append(new_item)
+        return new_item
+
+    # def remove(self, item):
+    #     self.inventory.remove(item)
+    #     return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+        # return item
