53e7c2b6bd7bc8b1a1e83ca649d1fd5a191420ec
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-03-31 14:55:22 -0700

    creates 'get_by_category' method for 'vendor' class and returns a list of the vendor's items based on an inputed category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,3 +14,10 @@ class Vendor():
         else:
             item = False
         return item
+    
+    def get_by_category(self, category):
+        items_in_category = []
+        for item in self.inventory:
+            if item.category == category:
+                items_in_category.append(item)
+        return items_in_category
