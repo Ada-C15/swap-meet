135367ae30804c8a7ac8087afe2e322899096a93
Rock - Viri Vazquez <viridiana.vzq@gmail.com>
2021-03-31 18:51:07 -0700

    Adding new files for swap_meet folder to create new classes. Vendor file created a class (Vendor) that has an inventory. Item file created a class (Item) that contains a category attribute. Test waves 1 and 2 are covered by these 2 classes.

---
+++ swap_meet/item.py
@@ -0,0 +1,12 @@
+class Item:
+    def __init__(self, category = None):
+        if category == None:
+            self.category = ""
+        else:
+            self.category = category
+    
+    
+
+
+
+
---
+++ swap_meet/vendor.py
@@ -0,0 +1,29 @@
+from .item import Item
+
+class Vendor(Item):
+    def __init__(self, inventory = None):
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+    
+    def get_by_category(self, category):
+        category_inventory = []
+        for item in self.inventory:
+            if item.category == category:
+                category_inventory.append(item)
+        return category_inventory
+
+
+
