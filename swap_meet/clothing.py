86f4f7a850c23f4968c2857a74260e05d6714d77
Brittney Payne <ohoptional@gmail.com>
2021-04-01 16:51:52 -0700

    defined classes and strings for clothing and decor

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
-class Clothing:
-    pass
+from .item import Item
+
+class Clothing(Item):
+    def __init__(self, category = None):
+        if category == None:
+            self.category = "Clothing"
+        else:
+            self.category = "Clothing"
+    
+
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,14 @@
-class Decor:
-    pass
+from .item import Item
+
+
+
+class Decor(Item):
+    def __init__(self, category = None):
+        if category == None:
+            self.category = "Decor"
+        else:
+            self.category = "Decor"
+    
+    def __str__(self):
+        return "Something to decorate your space."
+    
