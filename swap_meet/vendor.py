43153a065c8c060e657819ec101b5823220b53d8
mairandomness <mairakodama@gmail.com>
2021-04-01 10:27:38 -0700

    Add Vendor.swap_first_item(), Vendor.get_best_by_category() and Vendor.swap_best_by_category()

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,9 +13,9 @@ class Vendor:
         """
         PARAMETERS: list of items, optional (default is [])
         """
+        self.inventory = inventory
         if inventory is None:
             self.inventory = []
-        self.inventory = inventory
 
     def add(self, item):
         """
@@ -61,3 +61,41 @@ class Vendor:
         other.add(self.remove(own_item))
         self.add(other.remove(other_item))
         return True
+
+    def swap_first_item(self, other):
+        """
+        Makes an exchange with the first item in two Vendors inventories
+        PARAMETERS: other Vendor
+        OUTPUT: bool (represents if the swap was successful)
+        """
+        if not self.inventory or not other.inventory:
+            return False
+
+        return self.swap_items(other, self.inventory[0], other.inventory[0])
+
+    def get_best_by_category(self, category):
+        """
+        Gets item from vendor's inventory that matches a certain category and is
+        in best condition. If no item is found returns None.
+        PARAMETERS: category str
+        OUTPUT: Item or None
+        """
+        if not self.get_by_category(category):
+            return None
+
+        return max(self.get_by_category(category), key=lambda x: x.condition)
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        """
+        Swaps vendors items according to their category of preference and
+        best condition available
+        PARAMETERS: other Vendor, my_priority category str, their_priority category str
+        OUTPUT: bool (represents if the swap was successful)
+        """
+        they_have = other.get_best_by_category(my_priority)
+        i_have = self.get_best_by_category(their_priority)
+
+        if not i_have or not they_have:
+            return False
+
+        return self.swap_items(other, i_have, they_have)
