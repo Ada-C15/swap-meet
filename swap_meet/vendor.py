e8db50903c6daf84aa517f51758605c4167a201d
Sumitra <sumitrac2008@gmail.com>
2021-04-01 15:20:57 -0700

    updated

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,27 @@
 class Vendor:
-    pass
     def __init__(self, inventory = None):
         if inventory is None:
             self.inventory = []
         else: 
             self.inventory = inventory 
+        
+    def adding_to_inventory(Vendor):
+        if item in inventory:
+            self.inventory.add(item)
+        else:
+            self.inventory.remove(item)
+        return False 
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+        else:
+            return False 
+            
+        return item 
+
+
