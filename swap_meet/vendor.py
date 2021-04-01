09026525c770ec0adf5556177593fead3f19c22c
Sumitra <sumitrac2008@gmail.com>
2021-04-01 09:48:43 -0700

    updated

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,7 @@
 class Vendor:
     pass
+    def __init__(self, inventory = None):
+        if inventory is None:
+            self.inventory = []
+        else: 
+            self.inventory = inventory 
