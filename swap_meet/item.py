61dbb226952ba72888fa73d297d555c8bf165769
Courtney <courtneycmassey@gmail.com>
2021-04-01 12:34:24 -0700

    passing Wave03

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,10 @@
 class Item:
     
     def __init__(self, category=""):
-        if not category:
+        if category == "":
             self.category = ""
         else:
-            self.category = category 
+            self.category = category
+
+    def __str__(self):
+         return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -3,7 +3,7 @@ from swap_meet.item import Item
 class Vendor:
 
     def __init__(self, inventory=None):
-        if not inventory:
+        if inventory == None:
             self.inventory = []
         else:
             self.inventory = inventory 
@@ -25,3 +25,20 @@ class Vendor:
             if item.category == category:
                 items_in_category.append(item)
         return items_in_category
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        # if my_item in self.inventory AND their_item in other_vendor.inventory
+            # remove my_item from self.inventory
+            # and add my_item to other_ventory.inventory
+            # remove their_item from other_vendor.inventory
+            # and add their_item to self_inventory.inventory
+            # return True
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.inventory.remove(my_item)
+            other_vendor.add(my_item)
+            other_vendor.inventory.remove(their_item)
+            self.add(their_item)
+            return True
+        # else, return False
+        else:
+            return False
