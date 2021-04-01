e3b79db9b9c647f3c912781644a073ce0752993a
Saejin Kwak Tanguay <saejinkwak@gmail.com>
2021-04-01 09:48:14 -0700

    Wave 2: added class Item, added vendor method get_by_category which returns a list of Items with that category.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,13 +1,27 @@
 
 class Vendor:
-    def __init__ (self, inventory = []):
-        self.inventory = inventory
-    def add (self, item):
-        self.inventory.append(item)
+    def __init__ (self, inventory = None):
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+    def add (self, Item):
+        self.inventory.append(Item)
         return item
-    def remove(self, item):
-        if item in self.inventory:
-            self.inventory.remove(item)
-            return item
+    def remove(self, Item):
+        if Item in self.inventory:
+            self.inventory.remove(Item)
+            return Item
         else:
             return False
+
+    def get_by_category(self, category):
+
+        found_category_items = []
+
+        for Item in self.inventory:
+            if category == Item.category:
+                found_category_items.append(Item)
+        return found_category_items
+
+                
