d2827a4fc140c0e0ed9ed7b13b9416d593b6c1a1
Courtney <courtneycmassey@gmail.com>
2021-04-01 15:11:48 -0700

    passing first 3 tests of Wave05

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from .item import Item
+
+class Clothing(Item):
+
+    def __init__(self):
+        self.category = "Clothing"
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
+    
+    def condition_description(self, condition):
+        pass
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,15 @@
 class Item:
     
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=None):
         if category == "":
             self.category = ""
         else:
             self.category = category
+        
+        if condition == None:
+            self.condition = 0
+        else:
+            self.condition = condition 
 
     def __str__(self):
-         return "Hello World!"
+        return "Hello World!"
