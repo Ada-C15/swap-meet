6d8f7f375f7795f1f03cc96deecfc5e29942ffac
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-04-02 12:19:35 -0700

    passes wave 6 - creates 'swap_best-by-category' method for the 'Vendor' class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -50,3 +50,8 @@ class Vendor():
                     best_condition = item.condition
                     best_item = item
         return best_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+            my_item = self.get_best_by_category(their_priority)
+            their_item = other.get_best_by_category(my_priority)
+            return self.swap_items(other, my_item, their_item)
