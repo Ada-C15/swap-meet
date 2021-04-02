a91c169902036c84dae8ac883221e5150333adec
Whit Sundby <wksundby@gmail.com>
2021-04-01 19:41:10 -0700

    Adds condition attribute to Clothing, Electronics, Decor classes

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,7 +1,8 @@
 class Decor:
     
-    def __init__(self):
+    def __init__(self, condition=None):
         self.category = "Decor"
+        self.condition = condition
 
     def __str__(self):
         return "Something to decorate your space."
