ad3ead2558a4956dd5fd8e0196366ff8a97927fc
Sara <sarajessvega17@gmail.com>
2021-04-02 13:40:29 -0500

    refactor get_best_by_category()

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -5,7 +5,6 @@ from swap_meet.decor import Decor
 
 
 class Vendor:
-
     def __init__(self, inventory=None):
         if inventory == None:
             self.inventory = []
@@ -22,8 +21,7 @@ class Vendor:
         else:
             return False
         return item_name
-# Cd in terminal up 1 folder then run the specific 
-# file w/ python3 -m swap_meet.vendor
+
     def get_by_category(self, category):
         category_list = []
         for items in self.inventory:
@@ -46,52 +44,39 @@ class Vendor:
 
     def swap_first_item(self, other_vendor):
         if len(self.inventory) > 0 and len(other_vendor.inventory) > 0:
-            # remove my first item and add to other vendor
-            other_vendor.inventory.append(self.inventory[0])
-            self.inventory.remove(self.inventory[0])
-            # remove their first item and add to my invent. 
-            self.inventory.append(other_vendor.inventory[0])
-            other_vendor.inventory.remove(other_vendor.inventory[0])
+            self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
             return True
         else:
             return False
 
+
+
+
+
     def get_best_by_category(self, category):
+
         if len(self.inventory) == 0:
             return None
-        # Make a container for items that match category
-        category_finds = []
-        # loop thru invent to find matches and add to list
         
         for item in self.inventory:
-            if item.category == category:
-                category_finds.append(item)
-        if len(category_finds) < 1:
-            return None
-        
-        # compare ratings in finds list
-        best_so_far = category_finds[0]
-        if best_so_far.condition == 5:
-            return best_so_far
-        
-        for item in category_finds[1:]:
-            if item.condition > best_so_far.condition:
+            if item.category != category:
+                continue
+            if 'best_so_far' not in locals():
                 best_so_far = item
-                if best_so_far.condition == 5:
-                    return best_so_far
+            if item.condition <= best_so_far.condition:
+                continue
+            best_so_far = item
+        if 'best_so_far' not in locals():
+            return None
         return best_so_far
+
 
     def swap_best_by_category(self, other, my_priority, their_priority):
         my_best = self.get_best_by_category(their_priority)
         their_best = other.get_best_by_category(my_priority)
-        # print(my_best.condition, my_best.category, their_best.condition, their_best.category)
 
         if my_best != None and their_best != None:
-            other.inventory.append(my_best)
-            self.inventory.remove(my_best)
-
-            self.inventory.append(their_best)
-            other.inventory.remove(their_best)
+            self.swap_items(other, my_best, their_best)
             return True
         else:
             return False
