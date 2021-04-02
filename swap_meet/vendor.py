1484f26245f6d655304effc68130179be47268d9
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-04-02 12:02:54 -0700

    adds logic to 'get_best_by_category' method in 'Vendor' class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -42,4 +42,11 @@ class Vendor():
             return False
 
     def get_best_by_category(self, category):
-        pass
+        best_condition = 0
+        best_item = None
+        for item in self.inventory:
+            if item.category == category:
+                if item.condition > best_condition:
+                    best_condition = item.condition
+                    best_item = item
+        return best_item
