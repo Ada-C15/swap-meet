ae4fefcc8c9a797170340fbd6e89fb3ea48b52af
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-04-01 21:04:03 -0700

    creates decor.py file and creates 'Decor' class with accurate category and returns test string

---
+++ swap_meet/decor.py
@@ -0,0 +1,7 @@
+from swap_meet.item import Item
+class Decor(Item):
+    def __init__(self):
+        self.category = "Decor"
+    
+    def __str__(self):
+        return "Something to decorate your space."
