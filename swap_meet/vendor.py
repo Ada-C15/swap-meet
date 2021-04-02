57fe1b85b9f98afa2c3d5041ba41cdf5891609d9
Sumitra <sumitrac2008@gmail.com>
2021-04-02 13:11:20 -0700

    updated

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -6,18 +6,18 @@ class Vendor:
         else: 
             self.inventory = inventory 
         
-    def adding_to_inventory(Vendor):
+    def adding_to_inventory(Vendor): #checks item in inventory & returns True or False 
         if item in inventory:
             self.inventory.add(item)
         else:
             self.inventory.remove(item)
         return False 
     
-    def add(self, item):
+    def add(self, item): #append item in inventory 
         self.inventory.append(item)
         return item
 
-    def remove(self, item):
+    def remove(self, item): #remove item from inventory
         if item in self.inventory:
             self.inventory.remove(item)
         else:
@@ -26,11 +26,39 @@ class Vendor:
         return item 
 
     def get_by_category(self, category):
+        """
+        gets items by category
+        input: inventory from items
+        output: list of category 
+
+        """
         item_by_category = []
         for item in self.inventory:
             if item.category == category:
                 item_by_category.append(item)
         return item_by_category
+
+
+    def swap_items(self, vendor, my_item, their_item):
+        """
+        check items in Vendor, my_item, their_item
+        input:
+        output: 
+        """
+        for item in self.inventory:
+            if item == my_item:
+                self.remove(item)
+                vendor.add(item)
+                if their_item not in vendor.inventory:
+                    return False 
+                else: 
+                    self.add(their_item)
+                    vendor.remove(their_item)
+                    return True
+            else: 
+                return False
+        return False 
+
 
 
 
