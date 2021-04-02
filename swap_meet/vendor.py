bc2a4fcd05f344aa9cd97877f3079da9f1d66b77
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-04-02 11:14:42 -0700

    adds .add method and .remove method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,18 @@
 
 class Vendor:
     def __init__(self, inventory=None):
-        if not inventory:
+        if inventory == None:
             self.inventory = []
         else:
             self.inventory = inventory
+    
+    def add(self,item):
+        self.inventory.append(item)
+        return item
 
-# bob = Vendor()
-# pam = Vendor()
-# bob.inventory.append(["help"])
-
-# print(bob.inventory)
-# print(pam.inventory)
-
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
