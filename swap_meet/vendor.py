174d6fdba890acd19545e607da0b9a76780045b1
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-04-01 19:46:23 -0700

    creates 'swap_items' method for 'Vendor' class which swaps items between two vendor's inventories - meant to pass only first two tests of wave 3 but ended up passing all 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,3 +21,13 @@ class Vendor():
             if item.category == category:
                 items_in_category.append(item)
         return items_in_category
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.inventory.append(their_item)
+            self.inventory.remove(my_item)
+            other_vendor.inventory.append(my_item)
+            other_vendor.remove(their_item)
+            return True
+        else:
+            return False
