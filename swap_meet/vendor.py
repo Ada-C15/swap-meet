53f315ef4559f064627210c30c45776b8ab9cab8
alison <alimcginley@gmail.com>
2021-04-02 09:37:41 -0700

    adds docstrings to vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -2,17 +2,25 @@ from .item import Item
 import operator
 
 class Vendor:
+
+
     def __init__(self, inventory = None):
+    '''constructs instance of Vendor, initializing inventory with default None'''
+        
         if inventory == None:
             self.inventory = []
         else:
             self.inventory = inventory
 
     def add(self, item):
+    '''receives item as an argument and adds it to Vendor inventory'''
+        
         self.inventory.append(item)
         return item
 
     def remove(self, item):
+    '''receives item as an argument and removes it from Vendor inventory'''
+        
         if item in self.inventory:
             self.inventory.remove(item)
         else:
@@ -20,6 +28,9 @@ class Vendor:
         return item
     
     def get_by_category(self, category):
+    '''receives category as an argument,
+        creates and returns a list of items in that category'''
+
         items = []
         for item in self.inventory:
             if item.category == category:
@@ -27,6 +38,9 @@ class Vendor:
         return items
     
     def swap_items(self, other_vendor, item_a, item_b):
+    '''receives other vendor and items as arguments,
+        exchanges those items using add and remove methods'''
+
         if item_a in self.inventory and item_b in other_vendor.inventory:
             other_vendor.add(item_a)
             self.remove(item_a)
@@ -37,6 +51,8 @@ class Vendor:
             return False
         
     def swap_first_item(self, other_vendor):
+    '''exchanges first item in self.inventory with first item in other_vendor.inventory'''
+
         if self.inventory and other_vendor.inventory:
             self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
             return True
@@ -44,17 +60,21 @@ class Vendor:
             return False
     
     def get_best_by_category(self, category):
-        '''first calls get_by_category module
-            sorts items low to high by condition using operator.attrgetter'''
+        '''first calls get_by_category module with category as an argument,
+            sorts items low to high by condition using operator.attrgetter,
+            returns last/highest item in the sorted list'''
 
         items = self.get_by_category(category)
         if len(items) == 0:
             return None
-
         sorted_items = sorted(items, key=operator.attrgetter('condition'))
+
         return sorted_items[-1]
     
     def swap_best_by_category(self, other, my_priority, their_priority):
+    '''swaps best items from two vendors,
+        employs get_best_by_category, using opposing priorities as an argument'''
+        
         vendor_best = self.get_best_by_category(their_priority)
         other_vendor_best = other.get_best_by_category(my_priority)
         if other_vendor_best and vendor_best:
