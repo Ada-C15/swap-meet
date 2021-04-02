df4792a3c9b74f167e81c82f25f1a372e94dc061
Libby L. Gerber <libby@libbygerber.com>
2021-04-02 09:40:20 -0700

    added add method and remove method; passed all tests Wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,25 @@
 class Vendor:
-    def __init__(self):
-        self.inventory = []
+    def __init__(self, inventory = None):
+        if inventory == None:
+            self.inventory = []
+        else: 
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
+
+
+# inventory=[]
+
+# vendor_1 = Vendor(inventory)
+# print(vendor_1.inventory)
+# print(vendor_1.add("chocolate"))
+# print(vendor_1.inventory)
