987b22a974fff41380618f20b19e1789c2eb3214
Brittany Clemons <brittanyclemons.info@gmail.com>
2021-04-01 19:09:51 -0700

    Wave 2 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,24 @@
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
-        
+
     def add(self, new_item):
         self.inventory.append(new_item)
         return new_item 
         
         
     def remove(self, removed_item):
-        for i in self.inventory:
-            if i is removed_item:
+        for inventory_item in self.inventory:
+            if inventory_item is removed_item:
                 self.inventory.remove(removed_item)
                 return removed_item
         return False
+
+    def get_by_category(self, category = ""):
+        items = []
+        for inventory_item in self.inventory:
+            if(inventory_item.category is category):
+                items.append(inventory_item)
+        return items
+
+            
