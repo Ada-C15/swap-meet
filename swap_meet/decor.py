b51f7a6f84b8f94807760249e76fe14ff6cc64ee
alison <alimcginley@gmail.com>
2021-04-02 09:41:57 -0700

    adds docstrings to decor.py

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,11 +1,20 @@
 from .item import Item
 class Decor(Item):
     def __init__(self, condition =0):
+        '''constructs an instance of Decor,
+        which is an instance of Item,
+        with a default condition of 0'''
+
         super().__init__("Decor", condition)
        
     
     def __str__(self):
+        '''prints message for class'''
+
         return "Something to decorate your space."
     
     def condition_description(self):
+        '''prints instance condition,
+        employing condition_description from parent class (Item)'''
+        
         return super().condition_description()
