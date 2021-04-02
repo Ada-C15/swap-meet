ae86d3d1f2f2deea3c37b8fc0c5bb841ceb126f5
Hena <lee.henajane@gmail.com>
2021-04-01 21:20:40 -0500

    creates condition_description_summary in class Item that returns a summary of the item's condition

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,23 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
     
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        condition_decription_summary = ""
+        if self.condition >= 5.0:
+            condition_decription_summary = "Better than 5G."
+        elif self.condition >= 4.0:
+            condition_decription_summary = "Alright alright alright."
+        elif self.condition >= 3.0:
+            condition_decription_summary = "Good."
+        elif self.condition >= 2.0:
+            condition_decription_summary = "Not bad. But not good."
+        else:
+            condition_decription_summary = "Bad."
+        return condition_decription_summary
+
+
