b7345f16bd1a433caf337cd7ef6d4da1b94bee8d
Saejin Kwak Tanguay <saejinkwak@gmail.com>
2021-04-02 13:51:20 -0700

    Wave 6: added get_best_by_category Vendor method, added swap_best_by_category Vendor method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -52,4 +52,50 @@ class Vendor:
         else:
             
             return False
+
+    def get_best_by_category(self, category):
+        
+        max_condition = 0
+        best_category_Items = []
+        winner = None
+
+        for Item in self.inventory:
+            if Item.category == category:
+                best_category_Items.append(Item)
+            
+        if best_category_Items == None:
+            return None
+        
+        else:
+
+            for Item in best_category_Items:
+                if Item.condition > max_condition:
+                    max_condition = Item.condition
+
+            for best_item in best_category_Items:
+                if best_item.condition == max_condition:
+                    winner = best_item
+
+            return winner
                     
+    def swap_best_by_category (self, other, my_priority, their_priority):
+        if other.get_best_by_category(my_priority) == None:
+            return False
+        elif self.get_best_by_category(their_priority) == None:
+            return False
+
+        else:
+        
+            item_i_want = other.get_best_by_category(my_priority)
+            item_they_want = self.get_best_by_category(their_priority)
+
+            self.inventory.append(item_i_want)
+            other.inventory.append(item_they_want)
+            self.inventory.remove(item_they_want)
+            other.inventory.remove(item_i_want)
+
+            return True
+
+
+
+
