ebfd970a00f3cc8ee7cd7a9e92da6eb2381678b6
Whit Sundby <wksundby@gmail.com>
2021-04-01 19:48:55 -0700

    Adds condition_description method to Clothing, Electronics, and Decor classes

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -6,3 +6,15 @@ class Decor:
 
     def __str__(self):
         return "Something to decorate your space."
+
+    def condition_description(self):
+        if self.condition <= 1:
+            return "This is in terrible condition"
+        elif 1 < self.condition <= 2:
+            return "This is in poor condition"
+        elif 2 < self.condition <= 3:
+            return "This is in okay condition"
+        elif 3 < self.condition <= 4:
+            return "This is in good condition!"
+        else:
+            return "This is in excellent condition!"
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -6,3 +6,15 @@ class Electronics:
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
+
+    def condition_description(self):
+        if self.condition <= 1:
+            return "This is in terrible condition"
+        elif 1 < self.condition <= 2:
+            return "This is in poor condition"
+        elif 2 < self.condition <= 3:
+            return "This is in okay condition"
+        elif 3 < self.condition <= 4:
+            return "This is in good condition!"
+        else:
+            return "This is in excellent condition!"
