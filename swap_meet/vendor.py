6f7653154cea1df08d5964ad14f5b22983216143
Hannah Lumapas <lumapas.h@gmail.com>
2021-04-02 19:25:06 -0400

    takes in category and outputs list of items in that category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,10 +1,12 @@
+from swap_meet.item import Item
+
 class Vendor: 
     def __init__(self, inventory=None):
         if inventory == None: 
             self.inventory = []
         else: 
             self.inventory = inventory 
-            
+
     def add(self, item): 
             self.inventory.append(item)
             return item
@@ -15,3 +17,13 @@ class Vendor:
             return item
         else: 
             return False
+
+# ---- Wave 2 ----- #
+
+    def get_by_category(self, category): 
+        items_list = []
+        for item in self.inventory: 
+            if category == item.category: 
+                items_list.append(item)
+                print(items_list)
+        return items_list 
