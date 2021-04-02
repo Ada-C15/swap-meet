3706d179d685f29e59c45ba9d4dce87a97cd26d2
Kristin <Kristineliseboyd13@gmail.com>
2021-04-01 17:54:19 -0700

    all tests passed for second wave

---
+++ swap_meet/vendor.py
@@ -0,0 +1,23 @@
+# WAVE ONE
+
+class Vendor:
+    def __init__(self, inventory = []):
+        self.inventory = inventory 
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+        
+    def get_by_category(self, category):
+        items_by_category = []
+        for item in self.inventory:
+            if item.category == category:
+                items_by_category.append(item)
+        return items_by_category 
