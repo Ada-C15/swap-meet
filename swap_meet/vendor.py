be849fd7aa5fb5386c12c12bf6d93f7b0498a9a1
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-04-01 20:31:19 -0700

    creates the 'swap_first_item' method for the 'Vendor' class which swaps the first item between two vendors' inventories. Passes all tests in wave4.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -3,31 +3,40 @@ class Vendor():
         if inventory == None:
             inventory = []
         self.inventory = inventory
-    
+
     def add(self, new_item):
         self.inventory.append(new_item)
         return new_item
-    
+
     def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
         else:
             item = False
         return item
-    
+
     def get_by_category(self, category):
         items_in_category = []
         for item in self.inventory:
             if item.category == category:
                 items_in_category.append(item)
         return items_in_category
-    
+
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item in self.inventory and their_item in other_vendor.inventory:
             self.inventory.append(their_item)
             self.inventory.remove(my_item)
             other_vendor.inventory.append(my_item)
             other_vendor.remove(their_item)
+            return True
+        else:
+            return False
+
+    def swap_first_item(self, other_vendor):
+        if self.inventory and other_vendor.inventory:
+            # Using .pop() against Bertrand Meyer's express wishes \_('-')_/
+            self.inventory.append(other_vendor.inventory.pop(0))
+            other_vendor.inventory.append(self.inventory.pop(0))
             return True
         else:
             return False
