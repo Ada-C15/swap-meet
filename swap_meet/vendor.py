29e56e515f2c6e2b962f2a62887781ff6d6c6e10
Whit Sundby <wksundby@gmail.com>
2021-04-01 18:36:37 -0700

    Added fully functioning Vendor class and created Item class

---
+++ swap_meet/vendor.py
@@ -0,0 +1,23 @@
+class Vendor:
+    def __init__(self, inventory=None):
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return new_item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category):
+        for item in self.inventory:
+            if item.category == category:
+    
+    
