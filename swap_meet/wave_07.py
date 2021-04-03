400d14e2328bb0a3150443499a794b0daffe47d8
Jen Lim <jennifer.lim07@gmail.com>
2021-04-03 10:04:00 -0700

    added two more tests to wave_07.py

--- swap_meet/wave_07.py
+++ swap_meet/wave_07.py
@@ -34,3 +34,56 @@ def test_swap_by_newest():
     assert item_f in tai.inventory
     assert item_f not in jesse.inventory
     assert item_c in jesse.inventory
+
+
+def test_swap_by_newest_no_match_is_false():
+    tai = Vendor(
+    inventory=[]
+    )
+
+    item_a = Clothing(age="Vintage")
+    item_b = Decor(age="New")
+    item_c = Clothing(age="New")
+    jesse = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    result = tai.swap_by_newest(
+        other=jesse,
+        my_priority="Clothing",
+        their_priority="Decor"
+    )
+
+    assert result is False
+    assert len(tai.inventory) == 0
+    assert len(jesse.inventory) == 3
+    assert item_a in jesse.inventory
+    assert item_b in jesse.inventory
+    assert item_c in jesse.inventory
+
+
+def test_swap_by_newest_no_other_match():
+    item_a = Clothing(age="Vintage")
+    item_b = Decor(age="New")
+    item_c = Clothing(age="New")
+    tai = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    jesse = Vendor(
+        inventory=[]
+    )
+
+    result = tai.swap_by_newest(
+        other=jesse,
+        my_priority="Decor",
+        their_priority="Clothing"
+    )
+
+    assert result is False
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 0
+    assert item_a in tai.inventory
+    assert item_b in tai.inventory
+    assert item_c in tai.inventory
+
