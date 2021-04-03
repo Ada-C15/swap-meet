6688386317c370a1b07540162e15b19a92b03b46
SterlingSunshine <katrina.kimzey@gmail.com>
2021-04-03 16:14:12 -0700

    Wave 1 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,26 @@
+# Swap Meet
+# Ada Cohort 15 - Paper
+# Katrina Kimzey (she|they)
+# Started March 31, 2021
+
 class Vendor:
-    def __init__(self):
+    def __init__(self, inventory = None):
         # include an inventory
-        pass
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+        
 
     def add(self, item):
-        pass
+        # inventory entry
+        self.inventory.append(item)
+        return item
 
-    def 
+    def remove(self, item):
+        for thing in self.inventory:
+            if item == thing:
+                self.inventory.remove(thing)
+                return thing
+        else:
+            return False
