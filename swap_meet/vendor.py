60344cc912422b8423dac378d3090301b5f7f987
kmalakhova <kgrekhova@gmail.com>
2021-04-02 20:53:59 -0700

    Add get_newest_by_category(), swap_newest_by_category()

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,16 +1,14 @@
 class Vendor():
     def __init__(self, inventory = None):
-        if inventory == None:
-            self.inventory = []
-        else:
-            self.inventory = inventory
-        # self.inventory = inventory or []  
+        self.inventory = inventory or [] # [on_true] if [expression] else [on_false] 
 
     def add(self, item):
+        """Adds item to an inventory"""
         self.inventory.append(item)
         return item
 
     def remove(self, item):
+        """Removes item from an inventory"""
         if item in self.inventory: 
             self.inventory.remove(item)
             return item
@@ -18,6 +16,7 @@ class Vendor():
             return False        
 
     def get_by_category(self, category):
+        """Returns a list of items within a given category"""
         picked_items = []
         for item in self.inventory:
             if item.category == category:
@@ -25,6 +24,10 @@ class Vendor():
         return picked_items
 
     def swap_items(self, friend, my_item, their_item):
+        """
+        - Removes an item from Instances' inventory, adds it to a Friend's inventory
+        - Removes Friends item from a Friend's inventory, adds it to Instances' inventory
+        """
         if my_item in self.inventory and their_item in friend.inventory:
             self.add(their_item)
             self.remove(my_item)
@@ -35,29 +38,46 @@ class Vendor():
             return False
 
     def swap_first_item(self, friend):
-        if self.inventory and friend.inventory:
-            my_item = self.inventory[0]
-            their_item = friend.inventory[0]
-            self.remove(my_item)
-            friend.add(my_item)
-            friend.remove(their_item)
-            self.add(their_item)
-            return True
-        else:
+        """
+        Removes the first item from Instances' inventory, adds Friend's first item
+        Removes the first item from Friend's inventory, adds Instances first item
+        """
+        if len(self.inventory) == 0 or len(friend.inventory) == 0:
             return False
+        return self.swap_items(friend, self.inventory[0], friend.inventory[0])
 
     def get_best_by_category(self, category):
+        """Returns highest rated item in a given category"""
         items = self.get_by_category(category)
         if len(items) == 0:
             return None
-        best = items[0]
+        best_item = items[0]
         for item in items:
-            if item.condition > best.condition:
-                best = item
-        return best
+            if item.condition > best_item.condition:
+                best_item = item
+        return best_item
 
     def swap_best_by_category(self, other, my_priority, their_priority):
+        """Swaps highest rated items within a given category"""
         my_best_item = self.get_best_by_category(their_priority)
         others_best_item = other.get_best_by_category(my_priority)
 
         return self.swap_items(other, my_best_item, others_best_item)
+
+    def get_newest_by_category(self, category):
+        """Returns newest item in a given category"""
+        items = self.get_by_category(category)
+        if len(items) == 0:
+            return None
+        newest_item = items[0]
+        for item in items:
+            if item.age < newest_item.age:
+                newest_item = item
+        return newest_item
+
+    def swap_newest_by_category(self, friend, age, category):
+        """Swaps newest items within a given category"""
+        newest_item = self.get_newest_by_category(category)
+        friend_newest_item = friend.get_newest_by_category(category)
+
+        return self.swap_items(friend, newest_item, friend_newest_item)
