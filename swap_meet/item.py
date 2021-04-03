387df7c455ef808d890e7c6fb678043ae760f8ac
Marisa <marisa.n.morales@gmail.com>
2021-04-03 14:42:06 -0700

    Added files and updated main to pass wave 1

---
+++ swap_meet/item.py
@@ -0,0 +1,8 @@
+class Item:
+    def __ini__(self, category = ""):
+        self.category = category
+        return something
+    
+    def vendor.get_by_category 
+    means that I need to create
+    an instance method in vendor
---
+++ swap_meet/vendor.py
@@ -0,0 +1,18 @@
+class Vendor:
+    def __init__(self, inventory = None):
+        if inventory == None:
+            self.inventory = []
+        else: 
+            self.inventory = inventory
+
+    def add(self, item):
+        if item not in self.inventory:
+            self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
