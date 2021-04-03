fe6501269b037d25e015bfa6a44a0568ec027abd
Hena <lee.henajane@gmail.com>
2021-04-03 12:45:17 -0500

    refactors conditional_description method to dry out code

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,23 +1,20 @@
 class Item:
-    def __init__(self, category = "", condition = 0):
+    def __init__(self, category = "", condition = 0, age  = 0):
         self.category = category
         self.condition = condition
+        self.age = age
     
     def __str__(self):
         return "Hello World!"
 
     def condition_description(self):
-        condition_decription_summary = ""
         if self.condition >= 5.0:
-            condition_decription_summary = "Better than 5G."
+            return "Better than 5G."
         elif self.condition >= 4.0:
-            condition_decription_summary = "Alright alright alright."
+            return "Alright alright alright."
         elif self.condition >= 3.0:
-            condition_decription_summary = "Good."
+            return "Good."
         elif self.condition >= 2.0:
-            condition_decription_summary = "Not bad. But not good."
+            return "Not bad. But not good."
         else:
-            condition_decription_summary = "Bad."
-        return condition_decription_summary
-
-
+            return "Bad."
