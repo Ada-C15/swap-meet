84e8971c0a6cbde4f1097a51db124d3bc3b3318f
ggrossvi <hello.gloria@gmail.com>
2021-04-03 15:52:46 -0700

    added swap_items method- completed wave3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,4 @@
-from swap_meet.item import Item # Do I need this
+# from .item import Item # Do I need this
 
 class Vendor:
     def __init__(self, inventory = None):
@@ -10,10 +10,20 @@ class Vendor:
         
     
     def add(self, item):
+        """
+        function: adds Item to inventory 
+        input: Item to add
+        output: Item (list)
+        """
         self.inventory.append(item)
         return item
 
     def remove(self, item):
+        """
+        function: removes Item from inventory 
+        input: Item to remove
+        output: Item list if True, or False
+        """
         
         try:
             self.inventory.remove(item)
@@ -22,8 +32,13 @@ class Vendor:
             return False
 
     def get_by_category(self, category):
+        """
+        function: checks Items are in inventory 
+        input: Self, Vendor and Items to swap
+        output: Booleon (True for success)
+        """
         by_category_list = []
-        print("Category:", category)
+        # print("Category:", category)
         for item in self.inventory:
             #print("item>>", item)
             if item.category == category:
@@ -31,4 +46,28 @@ class Vendor:
                 by_category_list.append(item)
         return by_category_list
         
-            
+   #wave 3  
+    def swap_items(self, vendor,my_item,vendor_item ): 
+        """
+        function: checks Items are in inventory 
+        input: Self, Vendor and Items to swap
+        output: Booleon (True for success)
+        """
+        #self is fatimah
+        # self.inventory #Fatimah
+        # print(vendor.inventory)
+        if vendor.inventory == []:
+            return False
+        if my_item in self.inventory and vendor_item in vendor.inventory:
+            self.add(vendor_item)
+            self.remove(my_item)
+            vendor.remove(vendor_item)
+            vendor.add(my_item)
+            return True
+        return False
+    
+    #wave 4
+
+    def swap_first_item():
+        pass
+        
