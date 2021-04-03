520b7261019a9ceaf115b69ce797d7c18888cae6
Raquel Mena <raquelmena03@gmail.com>
2021-04-03 15:37:42 -0500

    "updated decor, clothing, electronic class modules, created condition_description method"

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,26 @@
 
 class Item:
-    def __init__(self, category=""): #an inventory has to be passed in
-        
+    def __init__(self, category='', condition=0): #an inventory has to be passed in
         self.category = category
+        self.condition = condition
         #self.get_by_category
     def __str__(self):
         return 'Hello World!'
+
+    def condition_description(self):
+        # for items in self.category:
+            if self.condition == 0:
+                return "You probably want to use a glove for this one"
+            if self.condition == 1:
+                return "Heavily used."
+            if self.condition == 2:
+                return "Heavily used."
+            if self.condition == 3:
+                return "Standard wear and tear."
+            if self.condition == 4:
+                return "*Almost* like new."
+            if self.condition == 5:
+                return "Like new"
+            else:
+                return "nope"
+
