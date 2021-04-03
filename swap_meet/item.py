13d2af401590367c8f47f44ebffbfd9c298e595a
Aida <aida.rysbek@gmail.com>
2021-04-03 00:14:27 -0700

    Added logic to pass test 5
    - Create 3 new modules with 3 classes
    - Define an instance method "condition_description"
    to describe item based on the value
    - Use Inheritance
    - Use super() to return objects of parent Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,22 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
         
+    def condition_description(self):
+        if self.condition == 0:
+            return "Heavily used"
+        elif self.condition == 1:
+            return "Pretty used"
+        elif self.condition == 2:
+            return "Normal condition"
+        elif self.condition == 3:
+            return "OK"
+        elif self.condition == 4:
+            return "Good"
+        elif self.condition == 5:
+            return "Brand new"
 
