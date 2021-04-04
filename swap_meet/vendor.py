42da012c2a29df7885e84180890969b33dbd72dc
Aida <aida.rysbek@gmail.com>
2021-04-03 23:30:38 -0700

    Added logic to pass the last three tests in 6
    - Initialize a method "swap_best_by_category"
    to swap the best item of certain categories with another Vendor
    - DRYing up the code - use get_best_by_category() and swap_items()
    to reduce the amount of repetitive bits of code in my inctance method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -64,5 +64,14 @@ class Vendor:
 
         return goodie
 
+    
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
+
+        if their_best_item == None or my_best_item == None:
+            return False
+        return self.swap_items(other, my_best_item, their_best_item)
+
 
 
