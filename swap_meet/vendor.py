fb2a06b32b4982df91d65c8c9dd1714c3685c480
Juliana DeCarvalho Sutherland <juliana.decarvalho@wsu.edu>
2021-04-03 20:21:38 -0700

    Wave #6, first 3 tests passing. Created get_best_by_category in vendor.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -40,3 +40,40 @@ class Vendor:
         if len(self.inventory) > 0 and len(vendor.inventory) > 0:
             return self.swap_items(vendor, self.inventory[0], vendor.inventory[0])
         return False 
+
+    def get_best_by_category(self, category):
+        highest_condition_item = Item(category)
+        item_exists = False
+        for item in self.inventory:
+            if item.category == category and item.condition > highest_condition_item.condition:
+                highest_condition_item = item 
+                item_exists = True
+        if not item_exists: return None
+        return highest_condition_item
+
+
+
+            
+
+### Wave 6
+
+# The first three tests in wave 6 imply:
+
+# - `Vendor`s have a method named `get_best_by_category`, which will get the item with the best condition in a certain category
+#   - It takes one argument: a string that represents a category
+#   - This method looks through the instance's `inventory` for the item with the highest `condition` and matching `category`
+#     - It returns this item
+#     - If there are no items in the `inventory` that match the category, it returns `None`
+#     - It returns a single item even if there are duplicates (two or more of the same item with the same condition)
+
+# The last three tests in wave 5 imply:
+
+# - `Vendor`s have a method named `swap_best_by_category`, which will swap the best item of certain categories with another `Vendor`
+#   - It takes in three arguments
+#     - `other`, which represents another `Vendor` instance to trade with
+#     - `my_priority`, which represents a category that the `Vendor` wants to receive
+#     - `their_priority`, which represents a category that `other` wants to receive
+#   - The best item in my inventory that matches `their_priority` category is swapped with the best item in `other`'s inventory that matches `my_priority`
+#     - It returns `True`
+#     - If the `Vendor` has no item that matches `their_priority` category, swapping does not happen, and it returns `False`
+#     - If `other` has no item that matches `my_priority` category, swapping does not happen, and it returns `False`
