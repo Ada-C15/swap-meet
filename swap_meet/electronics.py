fb22f452c031c1b70906eba85e7e35867e4828d3
Juliana DeCarvalho Sutherland <juliana.decarvalho@wsu.edu>
2021-04-03 20:03:42 -0700

    Wave #5 passed. I modified the classes to inherit item. And added condition_description method.

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,4 +1,13 @@
 from .item import Item
 
-class Eletronics:
-    pass
+class Electronics(Item):
+
+    def __init__(self, condition = 0):
+
+        super().__init__("Electronics", condition)
+
+    def __str__(self) -> str:
+        return "A gadget full of buttons and secrets."  
+
+    def condition_description(self):
+        return super().condition_description()
