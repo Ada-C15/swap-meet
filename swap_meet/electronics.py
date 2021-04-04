73c8d7d7e8ccec26c060cda3f45c1e070370a5e8
Priscille Gwet <ngomakak@gmail.com>
2021-04-03 18:21:17 -0700

    add 3 new modules: clothing.py,decor.py and electronics.py.

---
+++ swap_meet/electronics.py
@@ -0,0 +1,10 @@
+from swap_meet.item import Item
+
+
+class Electronics(Item):
+    
+    def __init__(self, category="Electronics", condition=0):
+        super().__init__(category, condition)
+    
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
