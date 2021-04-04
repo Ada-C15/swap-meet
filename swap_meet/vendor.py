e67fd885b8f0266cfde85923f99c111a1a7b77e1
Kaylyn Cardella <kaylyncardella@gmail.com>
2021-04-03 20:02:29 -0700

    Refactored swap_first_item to utilize swap_items in vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -39,15 +39,8 @@ class Vendor:
         if len(self.inventory) >= 1 and len(vendor.inventory) >= 1:
             my_first_item = self.inventory[0]
             friend_first_item = vendor.inventory[0]
-
-            vendor.inventory.insert(1, my_first_item)
-            self.inventory.insert(1, friend_first_item)
-
-            del self.inventory[0]
-            del vendor.inventory[0]
-
-            return True
-
+            return self.swap_items(vendor, my_first_item, friend_first_item)
+            
         return False
 
     def get_best_by_category(self, category):
