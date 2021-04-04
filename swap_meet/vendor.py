e7deb5680f0e77741a1193dd7ea5bc029b94d0c9
SterlingSunshine <katrina.kimzey@gmail.com>
2021-04-03 18:35:55 -0700

    Drying up code

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -47,10 +47,7 @@ class Vendor:
         
     def swap_first_item(self, friend):
         if len(self.inventory) and len(friend.inventory):
-            friend.add(self.inventory[0])
-            self.add(friend.inventory[0])
-            friend.remove(friend.inventory[0])
-            self.remove(self.inventory[0])
+            self.swap_items(friend, self.inventory[0], friend.inventory[0])
             return True
         else:
             return False
@@ -78,4 +75,3 @@ class Vendor:
             return True
         else:
             return False
-            
