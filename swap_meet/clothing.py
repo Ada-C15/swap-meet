56f43d9dbd2626dab47f9e85ac6e0f71fae695e9
Nandita Gilroy <nandita.gilroy@gmail.com>
2021-04-04 15:07:30 -0700

    creates 'age' attribute for 'Items' class and implements it in the 'Clothing', 'Decor', and 'Electronics' classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 class Clothing(Item):
-    def __init__(self, category = None, condition = 0):
-        super().__init__("Clothing", condition)
+    def __init__(self, category = None, condition = 0, age = None):
+        super().__init__("Clothing", condition, age)
 
     def __str__(self):
         return "The finest clothing you could wear."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,7 +1,7 @@
 from swap_meet.item import Item
 class Electronics(Item):
-    def __init__(self, category = None, condition = 0):
-        super().__init__("Electronics", condition)
+    def __init__(self, category = None, condition = 0, age = None):
+        super().__init__("Electronics", condition, age)
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
