af83c739615ece50b388a98a7b9422f49c47e934
Priscille Gwet <ngomakak@gmail.com>
2021-04-03 18:14:02 -0700

    Add a condition attribute and a new feature to describe each item's condition

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,33 @@
 class Item:
 
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         if category is None:
             self.category = ""
         else:
             self.category = category
+        if condition is None:
+            self.condition = 0
+        else:
+            self.condition = condition
+
     
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+
+        if self.condition == 0:
+            return "Poor"
+        elif self.condition == 1:
+            return "Fair"
+        elif self.condition == 2:
+            return "Good"
+        elif self.condition == 3:
+            return "Very good"
+        elif self.condition == 4:
+            return "Like new"
+        elif self.condition == 5:
+            return "Brand new"
 
 
     
