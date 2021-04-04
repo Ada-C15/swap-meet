2ef0e659f38a58867125897c74f5055dc13a7bc4
Whit Sundby <wksundby@gmail.com>
2021-04-04 14:47:04 -0700

    adds test for get_newest method

--- tests/test_wave_07.py
+++ tests/test_wave_07.py
@@ -5,6 +5,19 @@ from swap_meet.clothing import Clothing
 from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
 
+def test_get_newest_returns_newest_item():
+    item_a = Decor(age=2)
+    item_b = Electronics(age=7)
+    item_c = Clothing(age=0.5)
+    johannes = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    result = johannes.get_newest()
+
+    assert result is item_c
+
+    
 def test_swap_by_newest():
     item_a = Decor(age=2)
     item_b = Electronics(age=7)
