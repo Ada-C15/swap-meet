3c386d4aa329ce5821e09abbbdc9de6d7183c92b
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-04-03 19:44:15 -0700

    adds docstring documentation & fixes method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,17 +5,25 @@ class Item:
         else:
             self.category = category
     
+    """
+    This method personalizes the output for when 
+    str() is called, and returns the provided message.
+    """
     def __str__(self):
         return "Hello World!"
     
+    """
+    This method returns a condition description depending
+    on the value of that specific item's condition.
+    """
     def condition_description(self):
-        if self.condition == 1.0:
+        if 1.0 > self.condition >= 0.0:
             return "Not in the best shape, but still usable!"
-        elif self.condition == 2.0:
+        elif 2.0 > self.condition >= 1.0:
             return "Has received a lot of love, and could definitely use more!"
-        elif self.condition == 3.0:
+        elif 3.0 > self.condition >= 2.0:
             return "In fair condition! Definitely not the worst thing on the shelf!"
-        elif self.condition == 4.0:
+        elif 4.0 > self.condition >= 3.0:
             return "Very lightly used, but not mint condition."
-        elif self.condition == 5.0:
+        elif 5.0 >= self.condition >= 4.0:
             return "Like new! It's your lucky day!!"
