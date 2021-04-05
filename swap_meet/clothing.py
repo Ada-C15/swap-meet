fade679b363f70b108e80a673fcbb1dba1e3a959
Ruthie Newman <ruthienewman@protonmail.com>
2021-04-05 16:57:06 -0700

    Add string method to Clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,5 +1,8 @@
-import swap_meet.item
+from swap_meet.item import Item
 
-class Clothing():
-    def __init__(self, category = "Clothing", condition = 0):
-        pass
+class Clothing(Item):
+    def __init__(self, condition = 0):
+        super().__init__("Clothing", condition)
+
+    def __str__(self):
+        return "The finest clothing you could wear."
