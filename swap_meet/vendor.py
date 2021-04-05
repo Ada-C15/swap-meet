7de7fed9b4c0224d57cb53f9c6bbc90f5ddc8455
Ruthie Newman <ruthienewman@protonmail.com>
2021-04-05 15:56:19 -0700

    Add __str__ and swap_first_item to vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,4 @@
-from swap_meet.item import Item
+import swap_meet.item
 
 class Vendor():
 
@@ -33,7 +33,42 @@ class Vendor():
                     category_list.append(vendor_item)
                 i+=1               
             return category_list
+    
+    def swap_items(self, other_vendor, self_item, other_item):
+        found_item_other = False
+        found_item_self = False
+        
+        if self_item not in self.inventory or other_item not in other_vendor.inventory:
+            return False
 
+        for item in other_vendor.inventory:
+            if item == other_item:
+                other_vendor.inventory.remove(item)
+                other_vendor.inventory.append(self_item)       
+            
+        for item in self.inventory:
+            if item == self_item:
+                self.inventory.remove(item)
+                self.inventory.append(other_item)
+        
+        return True
+    
+    def swap_first_item(self, other_vendor):
+
+        if len(other_vendor.inventory) == 0 or len(self.inventory) == 0: 
+            return False
+        
+        first_item_self = self.inventory[0]
+        first_item_other = other_vendor.inventory[0]
+
+        for item in other_vendor.inventory:
+            other_vendor.inventory[0] = first_item_self
+
+        for item in self.inventory:
+            self.inventory[0] = first_item_other
+            
+        return True
+        
 
 
     
