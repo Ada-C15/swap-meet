ede56d1876a9cfce0f560ef7bf700885671bb21d
Marisa <marisa.n.morales@gmail.com>
2021-04-05 16:47:23 -0700

    refactors swap_meet/item swap_meet/clothing swap_meet/electronics and swap_meet/decor to pass wave 5

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,10 @@
-class Decor:
-    pass
+from .item import Item
+class Decor(Item):
+    def __init__(self, category = "Decor", condition = None):
+
+        super().__init__(category = "", condition = None)
+        self.category = "Decor"
+        self.condition = condition
+    
+    def __str__(self):
+        return "Something to decorate your space."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,20 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = None):
         self.category = category
-        
+        self.condition = condition
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        if int(self.condition) == 0:
+            return "Are you sure you want to buy this?"
+        elif int(self.condition) == 1:
+            return "One person's trash is another's treasure..."
+        elif int(self.condition) == 2:
+            return "You like a project don't you?"
+        elif int(self.condition) == 3:
+            return "This is in great condition"
+        elif int(self.condition) == 4:
+            return "What a find!"
+        elif int(self.condition) == 5:
+            return "This is practically brand new!"
