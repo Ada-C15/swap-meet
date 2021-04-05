0f157fe6c91b63c6eba5820478b1864788542bef
Ruthie Newman <ruthienewman@protonmail.com>
2021-04-05 16:58:37 -0700

    Add string method to Decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,6 +1,8 @@
-import swap_meet.item
+from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, category = "Decor", condition = 0):
-        pass
-        
+    def __init__(self, condition = 0):
+        super().__init__("Decor", condition)
+    
+    def __str__(self):
+        return "Something to decorate your space."
