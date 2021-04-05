80feaec5930c18fe2c490c50a269f6d4474dc898
melissa-nguyen <n.melissa.25@gmail.com>
2021-04-05 14:32:57 -0700

    updated swap_first_item instance method so vendor inventory friends inventory could be swapped

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,16 +24,6 @@ class Vendor:
         return item_list
     
     def swap_items(self, vendor_friend, my_item, their_item):
-        # if my_item not in vendor_friend.inventory and my_item in self.inventory and 
-        # their_item in vendor_friend.inventory and their_item not in self.inventory:
-
-        #     self.remove(my_item)
-        #     vendor_friend.add(my_item)
-        #     vendor_friend.remove(their_item)
-        #     self.add(their_item)
-        #     return True
-        # else:
-        #     return False
         if my_item not in self.inventory or their_item not in vendor_friend.inventory:
             return False
         else:
@@ -42,6 +32,33 @@ class Vendor:
             vendor_friend.remove(their_item)
             self.add(their_item)
             return True
+    
+    def swap_first_item(self, vendor_friend):
+        if self.inventory and vendor_friend.inventory:
+            my_item = self.inventory[0]
+            their_item = vendor_friend.inventory[0]
+            self.swap_items(vendor_friend, my_item, their_item)
+            return True
+        else: 
+            return False
+        
+
+
+        
+            
+            
+        
+        
+
+
+
+
+            
+        
+
+            
+            
+
     
 
     
