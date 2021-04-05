d3956d78421cc53601db5fad83eebe27e5d3f1b6
mairandomness <mairakodama@gmail.com>
2021-04-05 16:14:23 -0700

    Clean up inherited function duplicate code

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -19,5 +19,3 @@ class Clothing(Item):
     def __str__(self):
         return "The finest clothing you could wear."
 
-    def condition_description(self):
-        return super().condition_description()
