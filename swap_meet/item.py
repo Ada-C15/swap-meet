7988db31bcb08089b8364d26e90b86fb2a5bdc6c
Mai Truong <truong.mt89@gmail.com>
2021-04-04 22:54:36 -0400

    Wave 3 & 4 all tests passed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,11 +2,5 @@ class Item:
     def __init__(self, category = ""):
         self.category = category
 
-    # def __str__(self):
-    #     return "Hellow World!"
-
-    # def condition_description(self):
-    #     condition_description = ""
-    #     if self.condition == 0.0:
-    #         condition_description = a
-    #     elif self.condition ==  
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,3 +24,53 @@ class Vendor:
             if item.category == category:
                 matching_items.append(item)
         return matching_items
+
+
+    def swap_items(self, vendor_friend, my_item, their_item):
+
+        if my_item not in vendor_friend.inventory and my_item in self.inventory and their_item in vendor_friend.inventory and their_item not in self.inventory:
+            self.remove(my_item)
+            vendor_friend.add(my_item)
+            vendor_friend.remove(their_item)
+            self.add(their_item)
+            return True
+        else: 
+            return False 
+
+    def swap_first_item(self, vendor_friend):
+
+        if self.inventory and vendor_friend.inventory:
+            my_item = self.inventory[0]
+            their_item = vendor_friend.inventory[0]
+            self.swap_items(vendor_friend, my_item, their_item)
+            return True
+        else:
+            return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def condition_description(self):
+    #     condition_description = ""
+    #     if self.condition == 0.0:
+    #         condition_description = a
+    #     elif self.condition ==
