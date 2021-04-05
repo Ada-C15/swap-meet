60ace77b289661fba7f37231d484da9446757109
KARLA TORRES <t.karla212@gmail.com>
2021-04-05 19:22:58 -0400

    Successful pass for all tests in  wave 6// still need to apply DRY principle

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -40,21 +40,25 @@ class Vendor:
             return True
         else:
             return False
-    
-
-
-
-
-
-
-
-
-#"""if my_item in self.inventory
-#           self.remove(my_item)
-#            vendor.add(my_item)
-#        if their_item in vendor.inventory:
-#            vendor.remove(their_item)
-#            self.add(their_item)"""
-            
+    def get_best_by_category(self, category):
+        items = self.get_by_category(category)
+        if len(items) == 0:
+            return None
+        best_by_category = items[0]
+        for item in items:
+            if item.condition > best_by_category.condition:
+                best_by_category = item
+        return best_by_category
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_item = self.get_best_by_category(their_priority)
+        their_item = other.get_best_by_category(my_priority)
+        if my_item and their_item:
+            other.inventory.remove(their_item)
+            self.inventory.remove(my_item)
+            self.inventory.append(their_item)
+            other.inventory.append(my_item)
+            return True
+        else:
+            return False
 
 
