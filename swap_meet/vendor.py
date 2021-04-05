6faa18e1c53042011992910fbf04b13e2bbbf290
Mai Truong <truong.mt89@gmail.com>
2021-04-04 23:08:03 -0400

    Refactor Wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -28,14 +28,15 @@ class Vendor:
 
     def swap_items(self, vendor_friend, my_item, their_item):
 
-        if my_item not in vendor_friend.inventory and my_item in self.inventory and their_item in vendor_friend.inventory and their_item not in self.inventory:
+        if my_item not in self.inventory or their_item not in vendor_friend.inventory: 
+            return False 
+        else:
             self.remove(my_item)
             vendor_friend.add(my_item)
             vendor_friend.remove(their_item)
             self.add(their_item)
             return True
-        else: 
-            return False 
+
 
     def swap_first_item(self, vendor_friend):
 
