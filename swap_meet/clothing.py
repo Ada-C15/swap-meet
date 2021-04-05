23a7d578422a04c0f302b235e36afac1ae4c1767
Kristin <Kristineliseboyd13@gmail.com>
2021-04-05 16:13:58 -0700

    completed clothing file

---
+++ swap_meet/clothing.py
@@ -0,0 +1,23 @@
+from swap_meet.item import Item
+
+class Clothing:
+    def __init__(self, category = "Clothing", condition = 0):
+            self.category = category 
+            self.condition = condition 
+
+    def __str__(self):
+        return "The finest clothing you could wear."
+
+    # def condition_description(self, condition = 0):
+    #     self.condition = ["Mint condition!", "Gently used", "Kinda broken but it'll do", "You don't really want this", "Burn it"]
+    #     if condition == 5:
+    #         return self.condition[0]
+    #     elif condition == 4:
+    #         return self.condition[1]
+    #     elif condition == 3:
+    #         return self.condition[2]
+    #     elif condition == 2:
+    #         return self.condition[3]
+    #     elif condition == 1:
+    #         return self.condition[4]
+    #     return condition 
