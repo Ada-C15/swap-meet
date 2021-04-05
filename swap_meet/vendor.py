d9d8042ffe70041670f5bfc80ad38de8b23ebf53
melissa-nguyen <n.melissa.25@gmail.com>
2021-04-05 08:25:11 -0700

    refactored swap_items instance method using False for the if statement and True for the else statement

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -6,15 +6,13 @@ class Vendor:
             self.inventory = inventory
     
     def add(self, item):
-        self.item = item
-        self.inventory.append(self.item)
-        return self.item
+        self.inventory.append(item)
+        return item
     
     def remove(self, item):
-        self.item = item
-        if self.item in self.inventory:
-            self.inventory.remove(self.item)
-            return self.item
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
         else:
             return False
     
@@ -24,6 +22,37 @@ class Vendor:
             if category == item.category:
                 item_list.append(item)
         return item_list
+    
+    def swap_items(self, vendor_friend, my_item, their_item):
+        # if my_item not in vendor_friend.inventory and my_item in self.inventory and 
+        # their_item in vendor_friend.inventory and their_item not in self.inventory:
+
+        #     self.remove(my_item)
+        #     vendor_friend.add(my_item)
+        #     vendor_friend.remove(their_item)
+        #     self.add(their_item)
+        #     return True
+        # else:
+        #     return False
+        if my_item not in self.inventory or their_item not in vendor_friend.inventory:
+            return False
+        else:
+            self.remove(my_item)
+            vendor_friend.add(my_item)
+            vendor_friend.remove(their_item)
+            self.add(their_item)
+            return True
+    
+
+    
+
+
+
+
+
+
+
+
 
 
 
