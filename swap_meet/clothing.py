2df92e2baf41f77ac7626cfb76865f3b25f9fade
marielaxcruz <marielacrtzcruz@gmail.com>
2021-04-05 20:53:12 -0700

    added str method

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,8 +1,14 @@
 # ----------- WAVE 4 -----------
-
-class Clothing:
+from swap_meet.item import Item
+class Clothing(Item):
     def __init__ (self):
+        super()
         self.category = "Clothing"
+        # self.condition = 0
     
     def __str__(self):
         return "The finest clothing you could wear."
+    # def condition_description(self,condition):
+    #     if item 
+
+
