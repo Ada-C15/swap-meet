76239a43573a3fe3f7863eac6209ca25f278ea6a
Hena <lee.henajane@gmail.com>
2021-04-06 11:10:48 -0500

    adds optional test wave to test age attribute

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -38,24 +38,11 @@ class Vendor:
         if not items_in_category:
             return None
         best_condition = -1
-        best_item = None
         for item in items_in_category:
             if item.condition > best_condition:
                 best_condition = item.condition
                 best_item = item
         return best_item
-
-    # def get_best_by_age(self, wanted_category):
-    #     items_in_category = self.get_by_category(wanted_category)
-    #     if not items_in_category:
-    #         return None
-    #     newest_age = 5
-    #     newest_item = None
-    #     for item in items_in_category:
-    #         if item.age < newest_age:
-    #             newest_age = item.age
-    #             newest_item = item
-    #     return newest_item
 
     def swap_best_by_category(self, other, my_priority, their_priority):
         my_best_item = self.get_best_by_category(their_priority)
@@ -64,13 +51,22 @@ class Vendor:
         if not my_best_item or not their_best_item:
             return False
         return self.swap_items(other, my_best_item, their_best_item)
-    
-    # def swap_newest_by_age(self, other, my_priority, their_priority):
-    #     my_newest_item = self.get_best_by_age(their_priority)
-    #     their_newest_item = other.get_best_by_age(my_priority)
 
-    #     if not my_newest_item or not their_newest_item:
-    #         return False
-    #     return self.swap_items(other, my_newest_item, their_newest_item)
+    def get_newest_by_category(self, wanted_category):
+        items_in_category = self.get_by_category(wanted_category)
+        if not items_in_category:
+            return None
+        newest_age = 99
+        for item in items_in_category:
+            if item.age < newest_age:
+                newest_age = item.age
+                newest_item = item
+        return newest_item
     
-    
+    def swap_newest_by_category(self, other, my_priority, their_priority):
+        my_newest_item = self.get_newest_by_category(their_priority)
+        their_newest_item = other.get_newest_by_category(my_priority)
+
+        if not my_newest_item or not their_newest_item:
+            return False
+        return self.swap_items(other, my_newest_item, their_newest_item)
