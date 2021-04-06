a5bcc7d049aecbed20269e33bfd43855b445af59
Drew Taylor <drewlynntaylor@gmail.com>
2021-04-05 22:16:07 -0700

    Created the skeleton for Wave 3 and 4

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,6 @@
 class Item:
     def __init__(self, category = ""):
         self.category = category
-        
+
+    def __str__(self) -> str:
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,3 +23,35 @@ class Vendor:
             if items.category == category:
                 category_list.append(items)
         return category_list
+
+#Wave 3 *****************************
+
+    def swap_items(self, friend_vendor, my_item, their_item):
+        friend = [their_item]
+        if my_item in self.inventory and their_item in friend:
+            self.inventory.remove(my_item)
+            friend.append(my_item)
+            self.inventory.remove(their_item)
+            friend_vendor.inventory.append(their_item)
+            return True
+        else:
+            return False
+
+
+#Wave 4 **********************************
+
+    def swap_first_item(self, friend):
+        friends_first = friend.inventory[0]
+        first = self.inventory[0]
+        if len(self.inventory) > 0 and len(friend.inventory) > 0:
+            self.inventory.remove(first)
+            friend.inventory.append(first)
+            friend.inventory.remove(friends_first)
+            self.inventory.append(friends_first)
+            return True
+        else:
+            return False
+
+
+
+#Wave 5 ***************************
