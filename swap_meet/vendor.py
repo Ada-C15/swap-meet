0d54db581a2f8d3ffdba1fc88f890450e5e33344
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-04-05 19:54:01 -0700

    all tests in all waves passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -23,9 +23,13 @@ class Vendor:
 
     def get_by_category(self, category):
         items_list = []
+        print(category)
         for item in self.inventory:
-            if item.category == category:
+            #print(item.category)
+            if str(item.category) == str(category):
+                print(item.category)
                 items_list.append(item)
+        print(items_list)
         return items_list
 
     def swap_items(self, vendor_friend, my_item, their_item):
@@ -50,19 +54,26 @@ class Vendor:
 
     def get_best_by_category(self, category):
         category_list = self.get_by_category(category)
+        #print(category_list)
         if len(category_list) >= 1:
             best_condition_item = category_list[0]
             for item in category_list:
                 if item.condition > best_condition_item.condition:
                     best_condition_item = item
+            #print(best_condition_item)
             return best_condition_item
         else:
             return None
         
     def swap_best_by_category(self, other, my_priority, their_priority):
-        my_best_item = self.get_best_by_category(my_priority)
-        their_best_item = other.get_best_by_category(their_priority)
+        #print(my_priority, their_priority)
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
         
+
+        # print(my_best_item)
+        # print(their_best_item)
+
         if my_best_item and their_best_item:
             self.swap_items(other, my_best_item, their_best_item)
             return True
