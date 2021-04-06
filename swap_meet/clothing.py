1a2f3d58d3cd4e086ab99579d538c70b2f6927aa
Kristin <Kristineliseboyd13@gmail.com>
2021-04-05 19:57:32 -0700

    saved

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,6 +1,6 @@
 from swap_meet.item import Item
 
-class Clothing:
+class Clothing(Item):
     def __init__(self, category = "Clothing", condition = 0):
             self.category = category 
             self.condition = condition 
@@ -8,16 +8,4 @@ class Clothing:
     def __str__(self):
         return "The finest clothing you could wear."
 
-    # def condition_description(self, condition = 0):
-    #     self.condition = ["Mint condition!", "Gently used", "Kinda broken but it'll do", "You don't really want this", "Burn it"]
-    #     if condition == 5:
-    #         return self.condition[0]
-    #     elif condition == 4:
-    #         return self.condition[1]
-    #     elif condition == 3:
-    #         return self.condition[2]
-    #     elif condition == 2:
-    #         return self.condition[3]
-    #     elif condition == 1:
-    #         return self.condition[4]
-    #     return condition 
+    
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,23 +1,9 @@
 from swap_meet.item import Item
 
-class Decor:
+class Decor(Item):
     def __init__(self, category = "Decor", condition = 0):
             self.category = category 
             self.condition = condition 
 
     def __str__(self):
         return "Something to decorate your space."
-
-    # def condition_description(self, condition = 0):
-    #     self.condition = ["Mint condition!", "Gently used", "Kinda broken but it'll do", "You don't really want this", "Burn it"]
-    #     if condition == 5:
-    #         return self.condition[0]
-    #     elif condition == 4:
-    #         return self.condition[1]
-    #     elif condition == 3:
-    #         return self.condition[2]
-    #     elif condition == 2:
-    #         return self.condition[3]
-    #     elif condition == 1:
-    #         return self.condition[4]
-    #     return condition 
