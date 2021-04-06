a11fde1143e7a2cfdd015c8c42eaab95c24a09b3
Ruthie Newman <ruthienewman@protonmail.com>
2021-04-05 17:00:41 -0700

    Add condition_description method to Item class in item.py

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -13,22 +13,24 @@ class Item():
     def __str__(self):
         return "Hello World!"
         
-    def item_condition(self, condition):
+    def condition_description(self):
+
         item_condition = ""
-        if condition == 0:
+
+        if self.condition == 0:
             item_condition = "The finest clothing you could wear"
             return item_condition
-        elif condition >= 1 and condition < 2:
-            item_condition = "Not bad"
+        elif self.condition >= 1 and self.condition < 2:
+            item_condition = "Pretty shabby"
             return item_condition
-        elif condition >= 2 and condition < 3:
-            item_condition = "Pretty good"
+        elif self.condition >= 2 and self.condition < 3:
+            item_condition = "Not too shabby"
             return item_condition
-        elif condition >= 3 and condition < 4:
-            item_condition = "Good condition"
+        elif self.condition >= 3 and self.condition < 4:
+            item_condition = "Moving on up"
             return item_condition
-        elif condition >= 4 and condition < 5:
-            item_condition = "Great Condition"
+        elif self.condition >= 4 and self.condition <= 5:
+            item_condition = "DAAAAANG"
             return item_condition
 
 
