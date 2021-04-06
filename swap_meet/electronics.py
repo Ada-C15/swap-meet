b2703e37bb438bfdfb749bf91a5290f6957a98f3
Ruthie Newman <ruthienewman@protonmail.com>
2021-04-06 16:27:16 -0700

    Add import statement to electronics.py

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,5 +1,8 @@
-import swap_meet.item
+from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, category = "Electronics", condition= 0):
-        pass
+    def __init__(self, condition = 0):
+        super().__init__("Electronics", condition)
+    
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
