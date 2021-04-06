82342d280a17907d3f31630e56cb04bcc7dcdc9c
Nataliia Getlin <natali.getlin@gmail.com>
2021-04-05 19:37:10 -0700

    Create function condition_description()

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -11,4 +11,17 @@ class Item:
         # return self.category
 
     def condition_description(self):
-        return str(self.condition)
+        description = ""
+        if self.condition == 0:
+            description = "Seems uselesss, unless ..."
+        elif self.condition == 1:
+            description = "Happy if you can give it a try!"
+        elif self.condition == 2:
+            description = "Used - Fair"
+        elif self.condition == 3:
+            description = "Used - Good"
+        elif self.condition == 4:
+            description = "Used - Like new"
+        elif self.condition == 5:
+            description = "New"
+        return description
