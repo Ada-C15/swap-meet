0622b870468b2687a3d237aa6d627f352d0b9c93
Maria Magana <maria.imagana15@gmail.com>
2021-04-05 22:03:17 -0700

    updated wave 3 and wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,4 @@
-from swap_meet.item import Item
-
+#wave 1
 class Vendor: 
     def __init__(self, inventory = None):
         if inventory is None:
@@ -24,3 +23,27 @@ class Vendor:
             if item.category == category:
                 category_list.append(item)
         return category_list
+
+#Wave 3
+    def swap_items(self, friend, my_item, their_item):
+        if my_item not in self.inventory or \
+            their_item not in friend.inventory:
+            return False
+
+        self.remove(my_item)
+        friend.add(my_item)
+        friend.remove(their_item)
+        self.add(their_item)
+
+        return True
+
+#Wave 4
+    def swap_first_item(self, friend):
+        if  len(self.inventory) < 1 or \
+                len(friend.inventory) < 1:
+            return False
+        my_first_item = self.inventory[0]
+        their_first_item = friend.inventory[0]
+        self.swap_items(friend, my_first_item, their_first_item)
+        return True 
+        
