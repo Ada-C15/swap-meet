e5355cc6d8a8275e4ad92c8f57fe1f663e48d641
marielaxcruz <marielacrtzcruz@gmail.com>
2021-04-05 18:08:29 -0700

    added a stringify method

---
+++ swap_meet/clothing.py
@@ -0,0 +1,8 @@
+# ----------- WAVE 4 -----------
+
+class Clothing:
+    def __init__ (self):
+        self.category = "Clothing"
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
