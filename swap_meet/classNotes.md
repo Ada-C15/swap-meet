536834e657149aaa2c558ebd01c959b418cb2dcb
Leilani Allen <leilani.allen90@gmail.com>
2021-04-06 17:35:10 -0700

    Final Swap-Meet Poject

--- swap_meet/classNotes.md
+++ swap_meet/classNotes.md
@@ -20,3 +20,31 @@ print("jill list", jill.inventory)
 
 0000"])
 print("cheese list", cheese.inventory)
+
+# becca notes on 
+
+class Currency:
+    def __init__(self, value, unit="dollar"):
+        self.value = value
+        self.unit = unit
+    
+    def converter(self, other_unit):
+        pass
+
+    def __lt__(self, other):
+        if self.value < other.value:
+            return True
+        else: 
+            return False
+
+    def summary(self):
+        return f"{self.value} {self.unit}s"
+
+
+five = Currency(5)
+one = Currency(1)
+
+print(one < five)
+
+print(str(five))
+print(five.summary())
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,18 @@
+
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
-    
+        self.condition = condition 
+
     def __str__(self):
         return "Hello World!"
 
-
-# When we stringify an instance of Item using str(), it returns "Hello World!"
-# This implies Item overrides its stringify method
-  
-
-
+    def condition_description(self):
+        if self.condition <= 0:
+            return "Bad Condition"
+        elif self.condition < 3:
+            return "Okay Condition"
+        elif self.condition < 4:
+            return "Lightly Used"
+        else:
+            return "New"
