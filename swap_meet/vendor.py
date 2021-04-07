1f3a144f07b779ad36593104c1629754dd847d76
marielaxcruz <marielacrtzcruz@gmail.com>
2021-04-06 17:48:57 -0700

    added a swap_best_by_category method that returns the best item from the respective category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,4 @@
 from swap_meet.item import Item
-# or is it from .item import Item
-# what is the difference between these two type of imports
 
 # ---------- Wave 1 -----------
 
@@ -43,8 +41,7 @@ class Vendor:
 
 
 # --------------- Wave 2 --------------
-# you need to call the item file in order to access the 
-# category list 
+# you need to call the item.py file in order to access the category list 
 # list of items in category list 
     def get_by_category(self,category):
         items = []
@@ -59,21 +56,18 @@ class Vendor:
 # ------------- Wave 4 -------------
 
     def swap_first_item(self,friend_vendor):
-        # if not self.inventory or not friend_vendor.inventory:
-        #     return False
         if len(self.inventory) == 0 or len(friend_vendor.inventory) == 0:
                 return False
         else:
             first_item = self.inventory[0]
             first_friend_item = friend_vendor.inventory[0]
-            # self.inventory.remove(first_item)
             self.swap_items(friend_vendor,first_item,first_friend_item)
             return True 
 
 
 
 # ------------- Wave 5 -------------
-# this is using inheritance and the super method
+# this is using inheritance/ child and parent class and the super method
 
 
 # ------------- Wave 6  -------------
@@ -93,21 +87,14 @@ class Vendor:
         
         return quality_item        
         
-    
     def swap_best_by_category(self,other,my_priority,their_priority):
-        # this will return the best item from my priority category
-        my_category = self.get_best_by_category(my_priority)
-        # this will return the best item from thier priority category
-        other_category = self.get_best_by_category(their_priority)
+        # this will return the best item in my inventory that matches their_priority category
+        my_category = self.get_best_by_category(their_priority)
+        # this will return the best item from other inventory that matches from my_priority category
+        other_category = other.get_best_by_category(my_priority)
+        # invoking the method swap items that will swap the best items in their respective categories 
+        
+        total = self.swap_items(other,my_category,other_category) 
 
-        # self.swap_items(friend_vendor,my_item,their_item):
-        if other_category not in self.inventory or my_category not in other.inventory: 
-            return False
-        else: 
-            self.inventory.remove(my_category) 
-            other.inventory.append(my_category)
-            other.inventory.remove(other_category) 
-            self.inventory.append(other_category)
-            return True      
-
+        return total
 
