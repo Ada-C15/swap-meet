4a7b99b07fae96d615d7196439b4c3bbef29a324
Hannah Lumapas <lumapas.h@gmail.com>
2021-04-06 23:55:23 -0400

    Adds new attribute age. Adds docstrings for better documentation.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,29 +1,46 @@
-#from .vendor import Vendor
 # ---- Wave 2 ----- #
 class Item:
-    def __init__(self, category="", condition = 0): 
+    '''
+    Represents an Item
+    Attributes: 
+        category (str)
+        condition (int/float)
+        age (int/float)
+    '''
+
+    def __init__(self, category="", condition=0, age=0): 
+        '''
+        Initializes attributes of Item
+        Parameters: 
+            category which is optional and empty "" by default
+            condition which is optional and 0 by default 
+            age which is optional and 0 by default 
+        '''
         self.category = category
         self.condition = condition 
+        self.age = age
 
 # ---- Wave 3 ----- #
     def __str__(self): 
+        '''
+        Stringifies an instance of Item 
+        '''
         return "Hello World!"
-    
-    # def __repr__(self): 
-    #     return f"Item(category='{self.category}')"
-        #copy and pasted into ur python code makea  similar object
 
 # ---- Wave 5 ----- #
     def condition_description(self):
-        condition_description = ""
+        '''
+        Returns description based on the item's condition
+        Returns: 
+            description (str) 
+        '''
         if self.condition < .9: 
-            condition_description = "You want this? 🤣"
+            return "You want this? 🤣"
         elif self.condition < 1.9: 
-            condition_description = "Great DIY project 🛠" 
+            return "Great DIY project 🛠" 
         elif self.condition < 2.9: 
-            condition_description = "Seen better days 👀"
+            return "Seen better days 👀"
         elif self.condition < 3.9: 
-            condition_description = "Minty 🌿"
+            return "Minty 🌿"
         elif self.condition <= 5.0: 
-            condition_description = "NEW ✨"
-        return condition_description
+            return "NEW ✨"
