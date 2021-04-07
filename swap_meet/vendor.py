38706c3e3cce9531af302351fdcc9c25a1965e29
Lin Zhang <linzhang294@gmail.com>
2021-04-06 23:25:46 -0400

    makes edits to wave 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,8 @@
-# from swap_meet.item import Item
+# import pytest
+# # from swap_meet.item import Item
+# from swap_meet.clothing import Clothing
+# from swap_meet.decor import Decor
+# from swap_meet.electronics import Electronics
 
 
 class Vendor:
@@ -19,8 +23,7 @@ class Vendor:
         if item in self.inventory:
             self.inventory.remove(item)
             return item
-        else:
-            return False
+        return False
 
 # ***WAVE 2***
 
@@ -40,22 +43,18 @@ class Vendor:
             Vendor.add(my_item)
             Vendor.remove(their_item)
             return True
-        else:
-            return False
+        return False
 
 # ***WAVE 4***
     def swap_first_item(self, Vendor):
-        # self.Vendor = Vendor
         if (self.inventory and Vendor.inventory):
             return self.swap_items(Vendor, self.inventory[0], Vendor.inventory[0])
         return False
 
 
-#  ***WAVE 5***
+#  ***WAVE 6***
 
-    def get_best_by_category(self, category):
-        categories_list = self.get_by_category(category)
-
+        # max((item.value for item in categories_list))
 
 # def get_by_category(self, category):
 #     items_matching_category = []
@@ -63,3 +62,33 @@ class Vendor:
 #         if category == item.category:
 #             items_matching_category.append(item)
 #     return items_matching_category
+
+
+    def get_best_by_category(self, category):
+        list = []
+        for item in self.get_by_category(category):
+            list.append(item.condition)
+        for item in self.get_by_category(category):
+            if item.condition == max(list):
+                return item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+
+        if (my_priority in self.condition) and (their_priority in item.condition):
+            self.swap_items(other, self.get_best_by_category(
+                my_priority), other.get_best_by_category(their_priority))
+            return True
+        return False
+
+        # if (my_priority in self.condition) and (their_priority in Vendor.condition):
+        #     get_best_by_category(my_priority)
+        #     get_best_by_category(their_priority)
+
+        # def swap_items(self, Vendor, my_item, their_item):
+        #     if (my_item in self.inventory) and (their_item in Vendor.inventory):
+        #         self.remove(my_item)
+        #         self.add(their_item)
+        #         Vendor.add(my_item)
+        #         Vendor.remove(their_item)
+        #         return True
+        #     return False
