fc30bf0e2b87af1960cd963d790a11a05da2f649
Drew Taylor <drewlynntaylor@gmail.com>
2021-04-06 20:39:02 -0700

    A fully functioning project *clap* *clap*

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -49,7 +49,24 @@ class Vendor:
         friend.add(first_item)
         
         return True
-        
-
 
 #Wave 6 ***************************
+
+    def get_best_by_category(self, category):
+        highest_condition = 0
+        highest_item = None
+        for item in self.inventory:
+            if item.category == category:
+                if item.condition > highest_condition:
+                    highest_condition = item.condition
+                    highest_item = item
+        return highest_item
+
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        best_in_my_inventory = self.get_best_by_category(their_priority)
+        best_item_in_other = other.get_best_by_category(my_priority)
+        if best_item_in_other == None or best_in_my_inventory == None:
+            return False
+        self.swap_items(other, best_in_my_inventory, best_item_in_other)
+        return True
