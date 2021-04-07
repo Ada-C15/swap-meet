e47334c11133ea129af6159c3986061fc2c07146
Hannah Lumapas <lumapas.h@gmail.com>
2021-04-06 23:57:15 -0400

    Refactored and added docstrings for better documenetation

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -2,12 +2,26 @@ from swap_meet.item import Item
 
 # ---- Wave 5 ----- #
 class Decor(Item):
-    def __init__(self, condition=0): 
-        # could use super here
-        super().__init__("Decor",condition)
-        # self.category = "Decor"
-        # self.condition = condition
+    '''
+    Represents a decor Item
+    Attributes: 
+        category (str)
+        condition (int/float)
+        age (int/float)
+    '''
+    def __init__(self, condition=0, age=0): 
+        '''
+        Inherits methods of the parent class Item
+        Parameters: 
+            category ("Decor")
+            condition (int/float)
+            age (int/float)
+        '''
+        super().__init__("Decor",condition, age)
 
     def __str__(self): 
+        '''
+        Stringifies an instance of Decor 
+        '''
         return "Something to decorate your space."
 
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,10 +2,26 @@ from swap_meet.item import Item
 
 # ---- Wave 5 ----- #
 class Electronics(Item):
-    def __init__(self, category="", condition=0): 
-        self.category = "Electronics"
-        self.condition = condition
+    '''
+    Represents a electronic Item
+    Attributes: 
+        category (str)
+        condition (int/float)
+        age (int/float)
+    '''
+    def __init__(self, category="", condition=0, age=0): 
+        '''
+        Inherits methods of the parent class Item
+        Parameters: 
+            category ("Electronics")
+            condition (int/float)
+            age (int/float)
+        '''
+        super().__init__("Electronics",condition, age)
 
     def __str__(self): 
+        '''
+        Stringifies an instance of Electronics
+        '''
         return "A gadget full of buttons and secrets."
     
