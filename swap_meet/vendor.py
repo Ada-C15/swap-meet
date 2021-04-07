de3cdc1b8a2450341a54622ef405249b6a8998e8
KARLA TORRES <t.karla212@gmail.com>
2021-04-06 23:05:11 -0400

    removed all unnecessary comments.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -34,11 +34,6 @@ class Vendor:
             my_first_item = self.inventory [0]
             their_first_item = vendor.inventory[0]
             return self.swap_items(vendor, my_first_item, their_first_item)
-            # self.inventory.remove(my_first_item)
-            # vendor.inventory.remove(their_first_item)
-            # vendor.inventory.append(my_first_item)
-            # self.inventory.append(their_first_item)
-            # return True
         else:
             return False
     def get_best_by_category(self, category):
@@ -55,11 +50,6 @@ class Vendor:
         their_item = other.get_best_by_category(my_priority)
         if my_item and their_item:
             return self.swap_items(other, my_item, their_item)
-            # other.inventory.remove(their_item)
-            # self.inventory.remove(my_item)
-            # self.inventory.append(their_item)
-            # other.inventory.append(my_item)
-            # return True
         else:
             return False
 
